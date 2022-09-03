5320ed74a09a0625b316ebf0182d3359c5e9d6b1
thaint <thai@emobi.biz>
2022-09-03 21:54:07 +0700

    init project

---
+++ src/main/java/com/example/hexagonal_with_tdd/HexagonalWithTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_with_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalWithTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalWithTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexagonal_with_tdd/HexagonalWithTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_with_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalWithTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
